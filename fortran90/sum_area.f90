eddb9440bf3ba96147de555aa3781754db7f67f3
Paulo Alexandre <palexandremello@gmail.com>
2017-12-14 17:20:53 -0200

    modificações

--- fortran90/sum_area.f90
+++ fortran90/sum_area.f90
@@ -1,23 +1,8 @@
-subroutine acum_chuva(matrix,rain_tot,t,x,y,z)
+subroutine print_dados(matrix,t,x,y,z)
 	integer :: t,x,y,z
-	real :: matrix(t,x,y), rain_tot(t,x,y)
-    
-
-    do i = 0,t        
-        
-        if (i .gt. 0) then
-          
-                matrix(j,:,:) = rain_tot(i,:,:) - rain_tot(i-1,:,:) 
-                j=j+1           
-            else
-                matrix(j,:,:) = rain_tot(i,:,:)
-                j=j+1
-
-
-            end if
-
-    
-    end do
+	real,intent(in) :: matrix(t,x,y)
+ 
+ write(*,*) "Teste"
 
  RETURN
-end subroutine acum_chuva
+end subroutine print_dados
